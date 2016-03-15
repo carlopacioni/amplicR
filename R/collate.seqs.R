71e4389b4ca162e3616f12a0489e7107eefe0341
Carlo Pacioni <carlo.pacioni@gmail.com>
2016-03-15 10:38:56 +0800

    collate.seqs now return a stable

--- R/collate.seqs.R
+++ R/collate.seqs.R
@@ -3,37 +3,44 @@
 
 #' Collate sequences
 #' 
-#' \code{collate.seqs} can be used to collate together sequences contained in 
-#' different fasta files with the same name (where, most likely, the file name 
-#' is the sample name where these sequences are coming from).
+#' \code{collate.seqs} can be used to collate together outputs from 
+#' \code{data.proc}.
 #' 
-#' This function was developed because, if \code{data.proc} is looped over a 
-#' number of different \code{bp} values for the same sample files, and the 
-#' results stored in several sub folders, you will end up with several fasta 
-#' files in each sub folder. Many of these files are generated from the same 
-#' samples. However, the sequences contained in each file will be different 
-#' because they have been processed using a different \code{bp} values. In such 
-#' cases you may want to collate together all the sequences from the same 
-#' sample. \code{collate.seqs} does exactly this. By passing the path to 
-#' \code{data.proc} outputs, \code{collate.seqs} will search for the sub folder 
-#' "Final_seqs", read the fasta files and collate together the ones that have 
-#' matching names.
+#' This function was developed because, if, for example, sequencing runs are 
+#' carried out for the same samples, different  \code{data.proc} outputs will 
+#' also exists and you may want to collate together all the sequences from the 
+#' same sample. \code{collate.seqs} does exactly this. By passing either the 
+#' path to, or a list of, \code{data.proc} outputs, \code{collate.seqs} collate 
+#' together the sequences.
 #' 
-#' \code{collate.seqs} assumes that the fasta files have extension .fasta.
+#' \code{collate.seqs} will generate an output that it is very similar to
+#' \code{data.proc} but with the combined seqs for the same samples.
 #' 
-#' \code{collate.seqs} assumes that the fasta files were generated with 
-#' \code{data.proc}, so it will look for a "Final_seqs" sub folders within the 
-#' \code{dirs} path. As long as the folder structure is respected (i.e. the 
-#' fasta files are in a sub folder "Final_seqs"), the files from the same 
-#' samples have matching names and .fasta is their extension, this function can
-#' still be used .
-#' 
-#' @param dirs A character vector with the path to the folders where the fasta 
-#'   files are contained (e.g. where the outputs from data.proc were saved. See 
-#'   details)
+#' @param ldproc If the data are already in R memory, a list whose element are
+#'   \code{data.proc} outputs.
+#' @param rdas.in If \code{ldproc=NULL}, a character vector with the path to the 
+#'   rda files where the outputs from data.proc that have to be collated were 
+#'   saved. 
 #' @param dir.out The directory where to save the results. If NULL (default) 
 #'   then it will be set to the working directory
-#' @return Saves sequences to file in fasta format
+#' @return Return a list with several elements:
+#'   
+#'   \itemize{ 
+#'   \item $lbysamples: A list whose elements are vectors with the unique 
+#'         sequences (names) for each sample and their abundance (values). 
+#'   \item $stable: The sequence table 
+#'   \item $seq_list: The sequences and matching
+#'   sequences IDs 
+#'   \item $call: The function call}
+#'   
+#'   Several files are also saved to disk, these include:
+#'   \itemize{ 
+#'   \item Seq_table.csv Sequence table
+#'   \item Seq_list.csv List of sequences and their matching IDs
+#'   \item collated.rda R data file containing the list returned by collate.seqs 
+#'      (see above)
+#'      }
+#' @import data.table
 #' @export
 #' @examples 
 #' # Select the directory where the example data are stored
@@ -45,41 +52,104 @@
 #' out140 <- paste(out, "out140", sep="/")
 #' HTJ.140 <- data.proc(example.data, out140, bp=140)
 #' 
-#' # Run data.proc with bp=140 and save in a sub folder
+#' # Make up a some new sequence to collate by running data.proc with bp=141 and 
+#' # saving in a sub folder
 #' out141 <- paste(out, "out141", sep="/")
 #' HTJ.141 <- data.proc(example.data, out141, bp=141)
 #' 
-#' # Collate seqs of matching file names across the two sub folders 
-#' # out140 & out141
-#' collate.seqs(dirs=c(out140, out141), out)
+#' # Collate seqs of the two objects HTJ.140 and HTJ.141 
+#' collate.seqs(ldproc=list(HTJ.140, HTJ.141), dir.out=out)
+#' 
+#' # As above but using the .rda files
+#' rdas <- paste(out, c("out140", "out141"), sep="/")
+#' collate.seqs(rdas.in=rdas, dir.out=out)
 #' 
 #' # Clean up the temp directory
 #' unlink(out, recursive=TRUE)
 
 
-collate.seqs <- function(dirs, dir.out=NULL) {
+collate.seqs <- function(ldproc=NULL, rdas.in=NULL, dir.out=NULL) {
   library(ShortRead)
   #----------------------------------------------------------------------------#
   # Helper functions
   #----------------------------------------------------------------------------#
-  list.fasta <- function(dir) {
-    list.files(paste(dir, "Final_seqs", sep="/"), "*.fasta$")
+  make.df <- function(el) {
+    df <- data.frame(sequence=names(el), abundance=el, row.names=NULL)
+    return(df)
   }
   
-  collate <- function(ffile, dirs, collate.out) {
-    for (i in seq_along(dirs)) {
-      seqs <- readFasta(paste(dirs[i], "Final_seqs", sep="/"), pattern=ffile)
-      writeFasta(seqs, file=paste(collate.out, ffile, sep="/"), mode="a")
+  extr.seqs <- function(l) {
+    ldf <- lapply(l[[1]], make.df)
+    return(ldf)
+  }
+  
+  extr.names <- function(l) {
+    snames <- names(l[[1]])
+    return(snames)
+  }
+  
+  combine.sample <- function(sample, l) {
+    ldf <- l[names(l) == sample]
+    dt <- rbindlist(ldf)
+    return(dt)
+  }
+  
+  #----------------------------------------------------------------------------#
+  call <- sys.call(1)
+  if(is.null(ldproc)) {
+    if(is.null(rdas.in)) stop("Please, pass a list of data.proc outputs or their path")
+    temp.space <- new.env()
+    ldproc <- list()
+    for (i in seq_along(rdas.in)) {
+      dproc <- load(rdas.in[[i]], temp.space)
+      ldproc[[i]] <- get(dproc, temp.space)
+    }
+    rm(temp.space)
+  } else {
+      if(length(ldproc) < 2) stop("ldproc must be a list of length > 1")
+  }
+  
+  if(is.null(dir.out)) dir.out <- getwd()
+  ##################
+  lseqs <- list()
+  z <- 0
+  for (i in seq_along(ldproc)) {
+    nsamples <- length(ldproc[[i]][[1]])
+    for (j in 1:nsamples) {
+      z <- z + 1
+      lseqs[[z]] <- make.df(ldproc[[i]][[1]][[j]])
     }
   }
-  #----------------------------------------------------------------------------#
-  if(is.null(dir.out)) dir.out <- getwd()
-  ffiles <- lapply(dirs, list.fasta)
-  ffiles <- Reduce(intersect, ffiles)
+  ##################
+  snames_all <- unlist(lapply(ldproc, extr.names))
+  names(lseqs) <- snames_all
+  
+  samples <- unique(snames_all)
+  lbysamples <- lapply(samples, combine.sample, lseqs)
+  names(lbysamples) <- samples
+  
+  stable <- makeSequenceTable(lbysamples)
+  seqs <- colnames(stable)
+  seq_names <- paste0("seq", 1:dim(stable)[2])
+  colnames(stable) <- seq_names
+  seq_list <- data.frame(seq_names, sequence=seqs)
+  
   collate.out <- paste(dir.out, "Collated_seqs", sep="/")
   dir.create(collate.out, recursive=TRUE, showWarnings=FALSE)
+  write.csv(stable, file=paste(collate.out, "Seq_table.csv", sep="/"))
+  write.csv(seq_list, file=paste(collate.out, "Seq_list.csv", sep="/"), 
+            row.names=FALSE)
   
-  lapply(ffiles, collate, dirs, collate.out)
+  fasta_fold <- "Final_seqs"
+  fasta_dir <- paste(collate.out, fasta_fold, sep="/")
+  table2fasta(stable, seq.list=seq_list, dir.out=fasta_dir)
+  
+  collated <- list(lbysamples=lbysamples, 
+                   stable=stable, 
+                   seq_list=seq_list, 
+                   call=call)
+  save(collated, file=paste(collate.out, "collated.rda", sep="/"))
+  return(collated)
 }
 
 
