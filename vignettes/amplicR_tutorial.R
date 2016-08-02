482ee1f40701df2c4ca119b4960d903aee4283d4
Carlo Pacioni <carlo.pacioni@gmail.com>
2016-08-02 13:26:28 +0800

    rm vignette

--- vignettes/amplicR_tutorial.R
+++
@@ -1,5 +0,0 @@
-## ---- echo=FALSE---------------------------------------------------------
-info.file <- "C:/Users/cpacioni/Documents/JD-trial/TestRawData/FTP63.csv"
-info_table <- read.csv(info.file)
-#knitr::kable(info_table[c(1:2, 29:30, 43:44, 58:59), c(5:6,8,10:12,14)])
-
